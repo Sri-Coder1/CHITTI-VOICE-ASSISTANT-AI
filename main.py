411a3df5bc53c1ee722afe4345136413f653ea6a
Sri-Coder1 <srivatsankith.2005@gmail.com>
2025-12-22 16:26:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import multiprocessing
 import eel
-from engine.features import *
-from engine.command import *
+from engine.speech import speak
+from engine.features import playAssistantSound
+from engine.universal import handleDynamicQuery  # The new logic
+import engine.command  # Ensure commands are loaded
+if __name__ == "__main__":
+    multiprocessing.freeze_support()
+    eel.init('www')
 
-eel.init('www')
+    @eel.expose
+    def process_user_query(query):
+        handleDynamicQuery(query)
 
-# Do NOT call playAssistantSound() or os.system here
-
-eel.start('index.html', mode='chrome', host='localhost', block=True)
+    eel.start('index.html', mode='chrome', size=(1200, 800))
